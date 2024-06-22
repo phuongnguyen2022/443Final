5f3a35946fd79b4149196c1964e3db8ea3cc7554
Đỗ Thụy Anh Tuyền <tuyen.dothuy.cit19@eiu.edu.vn>
2024-06-22 13:22:32 +0700

    commit

--- KFC/FastFoodWebApplication/obj/Debug/net7.0/FastFoodWebApplication.AssemblyInfo.cs
+++ KFC/FastFoodWebApplication/obj/Debug/net7.0/FastFoodWebApplication.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastFoodWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce4ced9e6ab137de1cb3ffc8a2ac846fae6307aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5759b18a6f7e4932ce19dc0fad0c079d54a2f38e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastFoodWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastFoodWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
