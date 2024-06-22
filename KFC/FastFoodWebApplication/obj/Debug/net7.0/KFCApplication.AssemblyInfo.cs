d144f7613e3e9606a29071f35fb20070b9a87db2
phuongnguyen2022 <109055872+phuongnguyen2022@users.noreply.github.com>
2024-06-22 13:35:05 +0700

    commit

--- KFC/FastFoodWebApplication/obj/Debug/net7.0/KFCApplication.AssemblyInfo.cs
+++ KFC/FastFoodWebApplication/obj/Debug/net7.0/KFCApplication.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KFCApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce4ced9e6ab137de1cb3ffc8a2ac846fae6307aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5759b18a6f7e4932ce19dc0fad0c079d54a2f38e")]
 [assembly: System.Reflection.AssemblyProductAttribute("KFCApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KFCApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
