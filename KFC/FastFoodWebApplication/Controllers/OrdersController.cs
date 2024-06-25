13ef30c8d57ff0cbe0c3196167867622b706c098
Đỗ Thụy Anh Tuyền <tuyen.dothuy.cit19@eiu.edu.vn>
2024-06-26 01:27:13 +0700

    Commit 0626

--- KFC/FastFoodWebApplication/Controllers/OrdersController.cs
+++ KFC/FastFoodWebApplication/Controllers/OrdersController.cs
@@ -1,32 +1,35 @@
-﻿using KFCApplication.Data;
+﻿using FastFoodWebApplication.Services;
+using KFCApplication.Data;
 using KFCApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using NuGet.ProjectModel;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
-using Enumerable = System.Linq.Enumerable;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using Newtonsoft.Json;
+using System.Web;
 using String = System.String;
-using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json.Linq;
-using Humanizer;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace KFCApplication.Controllers
 {
     public class OrdersController : Controller
     {
         private readonly KFCApplicationContext _context;
+        private readonly IConfiguration _config;
+        protected string tmnCode;
+        protected string hashSecret;
+        protected string vnpUrl;
+        protected string confirmPayment = "https://localhost:7109/vnpayAPI/PaymentConfirm";
 
-        public OrdersController(KFCApplicationContext context)
+        public OrdersController(KFCApplicationContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
+            tmnCode = _config["VNPAY:TmnCode"];
+            hashSecret = _config["VNPAY:HashSecret"];
+            vnpUrl = _config["VNPAY:VnpUrl"];
         }
         public async Task<IActionResult> Index(string OrderShippingStatus)
         {
@@ -122,7 +125,7 @@ namespace KFCApplication.Controllers
             var user = _context.Users.Include(u => u.Profile).SingleOrDefault(u => u.UserName == userName);
 
             List<OrderDetail> orderDetail = new List<OrderDetail>();
-            var order = await _context.Order.SingleOrDefaultAsync(c=> c.Id == orderId);
+            var order = await _context.Order.SingleOrDefaultAsync(c => c.Id == orderId);
             decimal total = 0;
             if (order != null)
             {
@@ -147,7 +150,7 @@ namespace KFCApplication.Controllers
             decimal total = listOrder.Sum(item => item.TotalPrice);
             List<Dictionary<object, object>> list = new List<Dictionary<object, object>>();
             var dish = await _context.Dish.ToListAsync();
-      
+
             string date = DateTime.Now.Year + "-" + DateTime.Now.Month + "-" + DateTime.Now.Day;
             ViewData["revenue"] = total;
             ViewData["count"] = listOrder.Count;
@@ -197,9 +200,17 @@ namespace KFCApplication.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Placeorder([Bind("Id,OrderDate,TotalPrice,shipping_status,UserId,Name,Address,PhoneNumber,voucherCode")] Order order, string name, string address, string phone, string voucherCode,string discountMember)
+        public async Task<IActionResult> Placeorder([Bind("Id,OrderDate,TotalPrice,shipping_status,UserId,Name,Address,PhoneNumber,voucherCode")] Order order, string name, string address, string phone, string voucherCode, string discountMember)
         {
 
+            return await CreateOrderDetails(name, address, phone, voucherCode, discountMember);
+
+
+        }
+
+        private async Task<IActionResult> CreateOrderDetails(string? name, string? address, string? phone, string voucherCode, string discountMember)
+        {
+            var order = new Order();
             string userName = User.Identity.Name;
             var user = _context.Users.Include(u => u.Profile).SingleOrDefault(u => u.UserName == userName);
             var listOrder = await _context.Cart.Include(c => c.Dish).Where(c => c.UserId == user.Id && c.Dish.DishStatus == 1).ToListAsync();
@@ -212,14 +223,14 @@ namespace KFCApplication.Controllers
             order.Address = address;
             order.PhoneNumber = phone;
             order.Name = name;
-            
+
             order.OderDate = DateTime.Now;
             var code = voucherCode.Split("-");
             var voucherID = int.Parse(code[1]);
             var voucherAmount = int.Parse(code[0]);
             var discountMemberShip = decimal.Parse(discountMember);
             var dis = total * voucherAmount / 100;
-            total =total-dis- discountMemberShip;
+            total = total - dis - discountMemberShip;
             if (voucherID != 0)
             {
                 DisableByVoucherID(voucherID, user.Id);
@@ -243,10 +254,10 @@ namespace KFCApplication.Controllers
                     orderDetail.Price = item.Price;
                     orderDetail.size = item.size;
 
-                    var sql = $"INSERT INTO OrderDetail (OrderId , DishId, Quantity, Price, size) VALUES ({id},{item.DishId}, {item.Quantity}, {item.Price}, '{item.size}')";
+                    var sql = $"INSERT INTO OrderDetail (OrderId , DishId, Quantity, Price, size) VALUES ({id},{item.DishId}, {item.Quantity}, {item.Price:#.##}, '{item.size}')";
                     await _context.Database.ExecuteSqlRawAsync(sql);
                     _context.Cart.Remove(item);
-                   
+
 
 
                 }
@@ -257,7 +268,6 @@ namespace KFCApplication.Controllers
             {
                 _context.Order.Remove(order);
             }
-
 
             return View();
         }
@@ -273,15 +283,22 @@ namespace KFCApplication.Controllers
         public async Task<IActionResult> Checkout()
         {
 
+            var cart = await GetCheckoutData();
+
+            return View(cart);
+        }
+
+        private async Task<List<Cart>> GetCheckoutData()
+        {
             string userName = User.Identity.Name;
             var user = _context.Users.Include(u => u.Profile).SingleOrDefault(u => u.UserName == userName);
             decimal userSpend = user.Profile.UserSpend;
             decimal discount = 0;
-           
 
-            var cart = _context.Cart.Include(c => c.Dish).Where(c => c.UserId == user.Id && c.Dish.DishStatus ==1).ToList();
 
-            var vouchers = await _context.UserVoucher.Include(c => c.Voucher).Include(c => c.User).Where(c => c.UserId == user.Id && c.VoucherStatus ==1).ToListAsync();
+            var cart = _context.Cart.Include(c => c.Dish).Where(c => c.UserId == user.Id && c.Dish.DishStatus == 1).ToList();
+
+            var vouchers = await _context.UserVoucher.Include(c => c.Voucher).Include(c => c.User).Where(c => c.UserId == user.Id && c.VoucherStatus == 1).ToListAsync();
             decimal total = cart.Sum(item => item.Price);
             if (userSpend >= 0 && userSpend < 3000000)
             {
@@ -291,11 +308,11 @@ namespace KFCApplication.Controllers
             {
                 discount = (total * (decimal)0.1);
             }
-            if (userSpend >= 5000000 && userSpend < 7000000 )
+            if (userSpend >= 5000000 && userSpend < 7000000)
             {
                 discount = (total * (decimal)0.15);
             }
-            if ( userSpend >= 7000000)
+            if (userSpend >= 7000000)
             {
                 discount = (total * (decimal)0.2);
             }
@@ -304,7 +321,7 @@ namespace KFCApplication.Controllers
             ViewData["membership"] = discount;
             ViewData["discount"] = total - discount;
 
-            return View(cart);
+            return cart;
         }
 
         public async Task<IActionResult> Report()
@@ -344,14 +361,14 @@ namespace KFCApplication.Controllers
             }
             for (int i = 0; i < dishType.Count; i++)
             {
-                
+
                 Dictionary<object, object> map = new Dictionary<object, object>();
                 var orderDetail = await _context.OrderDetail.Include(c => c.Order).
                     Include(c => c.Dish).ThenInclude(c => c.DishType).
                     Where(c => c.Order.shipping_status == "Completed"
                     && c.Order.OderDate.Date == DateTime.Now.Date && c.Dish.DishType.Id == dishType[i].Id).ToListAsync();
 
-                
+
                 if (!map.ContainsKey(dishType[i].Id))
                 {
                     map.Add("label", dishType[i].Name);
@@ -453,6 +470,80 @@ namespace KFCApplication.Controllers
             ViewData["FromDate"] = fromDate;
             ViewData["ToDate"] = toDate;
             return View();
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> Payment([Bind("Name,Address,PhoneNumber,voucherCode")] string name, string address, string phone, string voucherCode, string discountMember)
+        {
+            string hostName = System.Net.Dns.GetHostName();
+            string clientIPAddress = System.Net.Dns.GetHostAddresses(hostName).GetValue(0).ToString();
+            var pay = new PayLib();
+            await GetCheckoutData();
+            var value = decimal.TryParse(ViewData["discount"].ToString(), out decimal result);
+            pay.AddRequestData("vnp_Version", "2.1.0"); //Phiên bản api mà merchant kết nối. Phiên bản hiện tại là 2.1.0
+            pay.AddRequestData("vnp_Command", "pay"); //Mã API sử dụng, mã cho giao dịch thanh toán là 'pay'
+            pay.AddRequestData("vnp_TmnCode", tmnCode); //Mã website của merchant trên hệ thống của VNPAY (khi đăng ký tài khoản sẽ có trong mail VNPAY gửi về)
+            pay.AddRequestData("vnp_Amount", ((int)(result * 100)).ToString()); //số tiền cần thanh toán, công thức: số tiền * 100 - ví dụ 10.000 (mười nghìn đồng) --> 1000000
+            pay.AddRequestData("vnp_BankCode", ""); //Mã Ngân hàng thanh toán (tham khảo: https://sandbox.vnpayment.vn/apis/danh-sach-ngan-hang/), có thể để trống, người dùng có thể chọn trên cổng thanh toán VNPAY
+            pay.AddRequestData("vnp_CreateDate", DateTime.Now.ToString("yyyyMMddHHmmss")); //ngày thanh toán theo định dạng yyyyMMddHHmmss
+            pay.AddRequestData("vnp_CurrCode", "VND"); //Đơn vị tiền tệ sử dụng thanh toán. Hiện tại chỉ hỗ trợ VND
+            pay.AddRequestData("vnp_IpAddr", clientIPAddress); //Địa chỉ IP của khách hàng thực hiện giao dịch
+            pay.AddRequestData("vnp_Locale", "vn"); //Ngôn ngữ giao diện hiển thị - Tiếng Việt (vn), Tiếng Anh (en)
+            pay.AddRequestData("vnp_OrderInfo", $"{voucherCode}:{discountMember}:{name}:{address}:{phone}"); //Thông tin mô tả nội dung thanh toán
+            pay.AddRequestData("vnp_OrderType", "other"); //topup: Nạp tiền điện thoại - billpayment: Thanh toán hóa đơn - fashion: Thời trang - other: Thanh toán trực tuyến
+            pay.AddRequestData("vnp_ReturnUrl", confirmPayment); //URL thông báo kết quả giao dịch khi Khách hàng kết thúc thanh toán
+            pay.AddRequestData("vnp_TxnRef", Guid.NewGuid().ToString()); //mã hóa đơn
+
+            string paymentUrl = pay.CreateRequestUrl(vnpUrl, hashSecret);
+            return Redirect(paymentUrl);
+        }
+
+        [Route("/VNpayAPI/paymentconfirm")]
+        public async Task<IActionResult> PaymentConfirm()
+        {
+            if (Request.QueryString.HasValue)
+            {
+                //lấy toàn bộ dữ liệu trả về
+                var queryString = Request.QueryString.Value;
+                var json = HttpUtility.ParseQueryString(queryString);
+
+                var orderInfor = json["vnp_OrderInfo"].ToString().Split(":"); //Thông tin giao dịch
+                long vnpayTranId = Convert.ToInt64(json["vnp_TransactionNo"]); //mã giao dịch tại hệ thống VNPAY
+                string vnp_ResponseCode = json["vnp_ResponseCode"].ToString(); //response code: 00 - thành công, khác 00 - xem thêm https://sandbox.vnpayment.vn/apis/docs/bang-ma-loi/
+                string vnp_SecureHash = json["vnp_SecureHash"].ToString(); //hash của dữ liệu trả về
+                var pos = Request.QueryString.Value.IndexOf("&vnp_SecureHash");
+
+                //return Ok(Request.QueryString.Value.Substring(1, pos-1) + "\n" + vnp_SecureHash + "\n"+ PayLib.HmacSHA512(hashSecret, Request.QueryString.Value.Substring(1, pos-1)));
+                bool checkSignature = ValidateSignature(Request.QueryString.Value.Substring(1, pos - 1), vnp_SecureHash, hashSecret); //check chữ ký đúng hay không?
+                if (checkSignature && tmnCode == json["vnp_TmnCode"].ToString())
+                {
+                    if (vnp_ResponseCode == "00")
+                    {
+                        //Thanh toán thành công
+                        return await CreateOrderDetails(orderInfor[2], orderInfor[3], orderInfor[4], orderInfor[0], orderInfor[1]);
+                    }
+                    else
+                    {
+                        //Thanh toán không thành công. Mã lỗi: vnp_ResponseCode
+                        return Redirect("LINK");
+                    }
+                }
+                else
+                {
+                    //phản hồi không khớp với chữ ký
+                    return Redirect("đường dẫn nếu phản hồi ko hợp lệ");
+                }
+            }
+            //phản hồi không hợp lệ
+            return Redirect("LINK");
+
+        }
+        public bool ValidateSignature(string rspraw, string inputHash, string secretKey)
+        {
+            string myChecksum = PayLib.HmacSHA512(secretKey, rspraw);
+            return myChecksum.Equals(inputHash, StringComparison.InvariantCultureIgnoreCase);
         }
 
     }
